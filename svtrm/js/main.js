60651cb43285dc3b0fc0eed11c2cef6a00aa9c82
Brian Stoner <brianstoner@Brians-MacBook-Pro.local>
2016-02-13 19:33:14 -0500

    updates

--- svtrm/js/main.js
+++ svtrm/js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     var SCROLL_SPEED = 300;
 
     var scrollTos = {
-        '.header': '#about'
+        '.home': '#about'
     };
 
 
@@ -18,10 +18,64 @@ $(document).ready(function() {
     }
 
 
-    // set up the middle section carousel
+    // header
+    var $doc = $(document),
+        $header = $('.header'),
+        hasScrolledClass = 0,
+        now = 0,
+        lastUpdate = 0,
+        updateHeaderTimeout;
 
+    function updateHeader() {
+        var now = new Date().getTime();
 
-    // set up bag image cycle'ing
+        if (now - lastUpdate < 350) {
+            updateHeaderTimeout = setTimeout(updateHeader, 350);
+            return;
+        }
 
+        clearTimeout(updateHeaderTimeout);
 
+        if ($doc.scrollTop() > 200) {
+            if (!hasScrolledClass) {
+                $header.addClass('scrolled');
+                hasScrolledClass = 1;
+            }
+        } else if (hasScrolledClass) {
+            $header.removeClass('scrolled');
+            hasScrolledClass = 0;
+        }
+
+        lastUpdate = now;
+    };
+
+    $(window).on('scroll', updateHeader);
+
+    // read more section
+    var $readmore = $('.read-more'),
+        renderedVideo;
+
+    $('.btn--read-more').click(function(e) {
+      e.preventDefault();
+      if ($readmore.hasClass('is-showing')) {
+          $readmore.removeClass('is-showing');
+      } else {
+          $.scrollTo('.read-more', {
+              duration: 600,
+              offset: -56
+          });
+
+          setTimeout(function() {
+            $readmore.addClass('is-showing');
+          }, 300);
+
+          setTimeout(function() {
+            if (!renderedVideo) {
+              $('.read-more__video').html(
+                '<video autoplay muted loop src="img/making.mp4"></video>'
+              );
+            }
+          }, 800);
+      }
+    });
 });
